b1b2fa18766662823e3d4c1274d4e3b789b09c0a
Marigza <marigola@mail.ru>
2024-03-07 16:20:25 +0300

    feat: add properties to class

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { IsNotEmpty, IsString } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsString()
+  @IsNotEmpty()
+  oldPassword: string; // previous password
+
+  @IsString()
+  @IsNotEmpty()
+  newPassword: string; // new password
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+import { Exclude } from 'class-transformer';
+import { UserModel } from './user.model';
+import { IsUUID } from 'class-validator';
+
+export class User implements UserModel {
+  @IsUUID()
+  id: string;
+
+  login: string;
+  version: number;
+  createdAt: number;
+  updatedAt: number;
+  @Exclude()
+  password: string;
+
+  constructor(partial: Partial<User>) {
+    Object.assign(this, partial);
+  }
+}
