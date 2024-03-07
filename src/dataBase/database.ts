b7797477d6d3b2b2518a74dd0e411974922b2534
Marigza <marigola@mail.ru>
2024-03-07 16:21:58 +0300

    refactor: update naming

--- src/dataBase/database.ts
+++ src/dataBase/database.ts
@@ -2,9 +2,9 @@ import { Album } from 'src/album/entities/album.model';
 import { Artist } from 'src/artist/entities/artist.model';
 import { Favorites } from 'src/favs/entities/fav.model';
 import { Track } from 'src/track/entities/track.model';
-import { User } from 'src/user/entities/user.model';
+import { UserModel } from 'src/user/entities/user.model';
 
-export const users: User[] = [];
+export const users: UserModel[] = [];
 export const artists: Artist[] = [];
 export const albums: Album[] = [];
 export const tracks: Track[] = [];
