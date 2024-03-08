bc902d97c54c368c3a999949a64705aa01bf00e9
Marigza <marigola@mail.ru>
2024-03-08 11:55:27 +0300

    feat: install swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import 'dotenv/config';
 import { ValidationPipe } from '@nestjs/common';
@@ -7,6 +8,16 @@ const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Home library')
+    .setDescription('The Library API description')
+    .setVersion('1.0')
+    .addTag('library')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(PORT);
 }
