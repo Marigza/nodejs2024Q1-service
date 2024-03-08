8cf6837ed443a3c984e9715902ad3feb9a2c4e16
Marigza <marigola@mail.ru>
2024-03-08 11:56:00 +0300

    feat: update models

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,10 +1,19 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty({
+    example: 'userName',
+    description: 'user login',
+  })
   @IsString()
   @IsNotEmpty()
   login: string;
 
+  @ApiProperty({
+    example: 'secretKey',
+    description: 'user password',
+  })
   @IsString()
   @IsNotEmpty()
   password: string;
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,15 +1,44 @@
 import { Exclude } from 'class-transformer';
 import { UserModel } from './user.model';
 import { IsUUID } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class User implements UserModel {
+  @ApiProperty({
+    example: 'ddeb27fb-d9a0-4624-be4d-4615062daed4',
+    description: 'Unique ID',
+  })
   @IsUUID()
   id: string;
 
+  @ApiProperty({
+    example: 'userName',
+    description: 'user Name',
+  })
   login: string;
+
+  @ApiProperty({
+    example: 1,
+    description: 'version of updating user',
+  })
   version: number;
+
+  @ApiProperty({
+    example: 123123123123,
+    description: 'timestamp of creating user',
+  })
   createdAt: number;
+
+  @ApiProperty({
+    example: 123123123123,
+    description: 'timestamp of updating user',
+  })
   updatedAt: number;
+
+  @ApiProperty({
+    example: 'secretPassword',
+    description: 'user password',
+  })
   @Exclude()
   password: string;
 
