4ec38f214459be1c953c667e15c059c2212f1693
Marigza <marigola@mail.ru>
2024-03-08 12:04:28 +0300

    feat: update model

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,12 +1,21 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
 import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @ApiProperty({
+    example: 'oldSecret',
+    description: 'previous user password',
+  })
   @IsString()
   @IsNotEmpty()
   oldPassword: string; // previous password
 
+  @ApiProperty({
+    example: 'newSecret',
+    description: 'new user password',
+  })
   @IsString()
   @IsNotEmpty()
   newPassword: string; // new password
