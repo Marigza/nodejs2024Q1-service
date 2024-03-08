f7f397b17d6ab8fb18b5d6abf3dbb8a78a2fcb59
Marigza <marigola@mail.ru>
2024-03-08 19:53:49 +0300

    feat: update database

--- src/dataBase/database.ts
+++ src/dataBase/database.ts
@@ -1,13 +1,13 @@
 import { AlbumModel } from 'src/album/entities/album.model';
 import { ArtistModel } from 'src/artist/entities/artist.model';
 import { Favorites } from 'src/favs/entities/fav.model';
-import { Track } from 'src/track/entities/track.model';
+import { TrackModel } from 'src/track/entities/track.model';
 import { UserModel } from 'src/user/entities/user.model';
 
 export const users: UserModel[] = [];
 export const artists: ArtistModel[] = [];
 export const albums: AlbumModel[] = [];
-export const tracks: Track[] = [];
+export const tracks: TrackModel[] = [];
 export const favorites: Favorites = {
   artists: [],
   albums: [],
