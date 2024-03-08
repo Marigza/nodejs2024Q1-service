8f879c2ed05009f4fd8461036b509edcb03fbfd1
Marigza <marigola@mail.ru>
2024-03-08 19:54:31 +0300

    feat: add properties to track model

--- src/track/dto/create-track.dto.ts
+++ src/track/dto/create-track.dto.ts
@@ -1,1 +1,32 @@
-export class CreateTrackDto {}
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateTrackDto {
+  @ApiProperty({
+    example: 'TrackName',
+    description: 'track name',
+  })
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty({
+    example: 'ddeb27fb-d9a0-4624-be4d-4615062daed5',
+    description: `track's artist`,
+  })
+  artistId: string | null;
+
+  @ApiProperty({
+    example: 'ddeb27fb-d9a0-4624-be4d-4615062daed5',
+    description: `track's album`,
+  })
+  albumId: string | null;
+
+  @ApiProperty({
+    example: 256000,
+    description: 'track duration',
+  })
+  @IsNumber()
+  @IsNotEmpty()
+  duration: number;
+}
--- src/track/dto/update-track.dto.ts
+++ src/track/dto/update-track.dto.ts
@@ -1,4 +1,34 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTrackDto } from './create-track.dto';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateTrackDto extends PartialType(CreateTrackDto) {}
+export class UpdateTrackDto extends PartialType(CreateTrackDto) {
+  @ApiProperty({
+    example: 'TrackName',
+    description: 'track name',
+  })
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty({
+    example: 'ddeb27fb-d9a0-4624-be4d-4615062daed5',
+    description: `track's artist`,
+  })
+  artistId: string | null;
+
+  @ApiProperty({
+    example: 'ddeb27fb-d9a0-4624-be4d-4615062daed5',
+    description: `track's album`,
+  })
+  albumId: string | null;
+
+  @ApiProperty({
+    example: 256000,
+    description: 'track duration',
+  })
+  @IsNumber()
+  @IsNotEmpty()
+  duration: number;
+}
